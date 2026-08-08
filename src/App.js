651b788c25242fed2d4083a0f260c198405c57a6
roshanscipit <63459572+roshanscipit@users.noreply.github.com>
2026-08-08 14:33:41 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Signup from "./js/pages/Signup";
 import Dashboard from "./js/pages/Dashboard";
 import Roles from "./js/pages/Roles";
 import LearningPath from "./js/pages/LearningPath";
+import CareerAgent from "./js/pages/CareerAgent";
 import Companies from "./js/pages/Companies";
 import CompanyDetail from "./js/pages/ComapanyDetails";
 import MockTests from "./js/pages/MockTests";
@@ -56,6 +57,7 @@ function AppRouter() {
         <Route path="/learning-path" element={<ProtectedRoute><LearningPath /></ProtectedRoute>} />
         <Route path="/role/:roleId"  element={<ProtectedRoute><LearningPath /></ProtectedRoute>} />
         <Route path="/profile"       element={<ProtectedRoute><UserProfile /></ProtectedRoute>} />
+        <Route path="/career-agent"  element={<ProtectedRoute><CareerAgent /></ProtectedRoute>} />
         <Route path="/help"          element={<ProtectedRoute><HelpPage /></ProtectedRoute>} />
       </Routes>
       {showLayout && <Footer />}
